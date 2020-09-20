f022ba1882c500f5b37807f1da7af240873f7dc7
hsk9210 <hsk9210@hsk9210-Macmini.local>
2020-09-21 00:22:14 +0900

    testcode Plus (~from 09.20)

--- OOP/oopTest.js
+++ OOP/oopTest.js
@@ -21,3 +21,26 @@ let employee = {
 };
 console.log(employee.getWage());
 //////////////////
+
+var obj1 = { x: 10 };
+var obj2 = Object.create(obj1);
+
+var obj3 = Object.create(obj2);
+
+obj2.x = 20;
+
+var result = obj3.x + 10;
+console.log(obj3.x);
+
+
+function outerFn() {
+  let outerVar = 'outer';
+  console.log(outerVar);
+  function innerFn() {
+    let innerVar = 'inner';
+    console.log(innerVar);
+  }
+  return innerFn;
+}
+
+outerFn();
