dae2d3cf90faded87fe5ab2c360cf684f9b57d8e
hsk9210 <hsk9210@hsk9210-Macmini.local>
2020-09-24 23:19:03 +0900

    oop 프로토타입으로 재복습중

--- OOP/oopTest.js
+++ OOP/oopTest.js
@@ -10,7 +10,27 @@ function getWage(baseSalary, overtime, rate) {
 //////////////////////
 
 
-///////////////////객체지향.
+///////////////////객체지향. 구식.
+function Car(brand, name, color) {
+  //인스턴스가 만들어질 때 실행되는 코드
+  this.brand = brand;
+  this.name = name;
+  this.color = color;
+}
+Car.prototype.refuel = function () {
+  //연료 공급
+  console.log('refueled');
+}
+Car.prototype.drive = function () {
+  //운전을 구현하는 코드
+  console.log('drived');
+}
+
+let avante = new Car('hyundai', 'avante', 'black');
+let mini = new Car('bmw', 'mini', 'white');
+avante.refuel();
+console.log(avante.brand);
+
 let employee = {
   baseSalary: 30000,
   overtime: 10,
@@ -32,15 +52,3 @@ obj2.x = 20;
 var result = obj3.x + 10;
 console.log(obj3.x);
 
-
-function outerFn() {
-  let outerVar = 'outer';
-  console.log(outerVar);
-  function innerFn() {
-    let innerVar = 'inner';
-    console.log(innerVar);
-  }
-  return innerFn;
-}
-
-outerFn();
