03c277d688525847514e4c9625834f313d2e2a01
hsk9210 <hsk9210@gmail.com>
2021-05-06 18:09:39 +0900

    day43 fin( React 기초...여야 하는데 학습시간의 1/3이 퀴즈 하나로 자습이었다?)

---
+++ 더존비즈온/reactPrac(douzone)/day_43/src/App2.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App2() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          수정된 <code>src/App2.js</code> 입니다.
+        </p>
+        <a
+          className="App-link"
+          href="App.js"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          되돌아가기
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App2;
