fef01ddbd96b1a1587917861d533d5e04ef4f96d
hsk9210 <hsk9210@gmail.com>
2021-06-10 21:44:26 +0900

    day67 fin

---
+++ Douzone/SpringPrac(douzone)/oracle18ctest/src/main/java/com/example/oracle18ctest/Oracle18ctestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oracle18ctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oracle18ctestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oracle18ctestApplication.class, args);
+    }
+
+}
---
+++ Douzone/SpringPrac(douzone)/oracle18ctest/src/test/java/com/example/oracle18ctest/Oracle18ctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oracle18ctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oracle18ctestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
